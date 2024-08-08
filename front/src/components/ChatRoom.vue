187f5299d7e2bf8ad8c8b3b9e20caacbf96ccfc7
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-08-09 07:29:48 +0900

    修正
    修正

--- front/src/components/ChatRoom.vue
+++ front/src/components/ChatRoom.vue
@@ -7,6 +7,19 @@
         <strong>{{ message.sender_name }}:</strong> {{ message.content }}
       </li>
     </ul>
+    <form @submit.prevent="sendMessage">
+      <div>
+        <h3>名前</h3>
+        <input type="text" v-model="senderName" placeholder="名前を入力" required />
+      </div>
+      <div>
+        <h3>メッセージ</h3>
+        <input type="text" v-model="newMessageContent" placeholder="メッセージを入力" required />
+      </div>
+      <div>
+        <button type="submit">送信</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -19,6 +32,8 @@ export default {
     return {
       roomName: '',
       messages: [],
+      senderName: '', // 追加
+      newMessageContent: '', // 追加
     };
   },
   created() {
@@ -36,5 +51,20 @@ export default {
         });
     },
   },
+  // 以下を追加
+    sendMessage() {
+      axios
+        .post(`http://localhost:3000/rooms/${this.roomId}/messages`, {
+          content: this.newMessageContent,
+          sender_name: this.senderName
+        })
+        .then(() => {
+          this.newMessageContent = ''
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }
 };
 </script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './routers';
+import router from './router';
 
 const app = createApp(App);
 app.use(router);
