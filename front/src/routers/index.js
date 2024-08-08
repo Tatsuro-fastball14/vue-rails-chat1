e39cf4aeba3ced0ffd605814e848bcd477a66536
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-08-08 13:20:52 +0900

    fix
    fix

--- front/src/routers/index.js
+++ front/src/routers/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ChatRooms from '../components/ChatRooms.vue';
 import ChatRoom from '../components/ChatRoom.vue'; // 追加
 
 const routes = [
-  { path: '/', component: ChatRooms },
   { path: '/rooms/:id', component: ChatRoom, props: route => ({ roomId: route.params.id }) }, // 追加
 ];
 
